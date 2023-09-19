cdb57241ce8a8ee4899ef1a522a028df921b6bef
Billy Rennekamp <billy.rennekamp@gmail.com>
2023-09-19 12:55:11 +0200

    bundled with original libraries

---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const LicenseWebpackPlugin = require('license-webpack-plugin').LicenseWebpackPlugin;
+
+const webpack = require('webpack');
+
+// const fs = require('fs');
+
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    R4AW: [path.resolve(__dirname, "src", "r4aw.js")],
+  },
+  externals: {
+    // 'fs': 'fs',
+    // 'window': 'window'
+  },
+  output: {
+    publicPath: '',
+    clean: true,
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    // static: path.resolve(__dirname, "dist"),
+    open: true,
+    liveReload: true,
+  },
+  node: {
+    __dirname: false,   // this configuration ensures you get the actual directory path
+    __filename: false,  // this configuration ensures you get the actual file path
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'R4AW',
+      metaDesc: 'R4AW',
+      template: path.resolve(__dirname, "src/index.ejs"),
+      filename: 'index.html',
+      inject: false,
+      minify: true,
+      templateParameters: (compilation, assets, options) => {
+        // const fs = require('fs');
+        // const p5Content = fs.readFileSync('./public/p5.min.js', 'utf-8');
+        return {
+          compilation,
+          webpackConfig: options.webpackConfig,
+          assets,
+          options,
+          // fs,
+          // p5Content,
+        };
+      },
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'R4AW',
+      metaDesc: 'R4AW',
+      template: path.resolve(__dirname, "src/nft.ejs"),
+      filename: 'nft.html',
+      inject: false,
+      minify: false,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public' }
+      ]
+    }),
+    // new webpack.BannerPlugin({
+    //   banner: `
+    //     My Project
+    //     Copyright (c) 2021 My Company
+    //     Licensed under the MIT License
+    //   `,
+    //   entryOnly: true
+    // }),
+    new LicenseWebpackPlugin({
+      outputFilename: 'licenses.txt',
+      perChunkOutput: false
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|ttf)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: true,
+            },
+          },
+        ],
+      },
+    ]
+  }
+}
